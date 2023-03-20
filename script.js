1b399fe324d53bb623aa9660f7676d1541e9ac3b
vrkljam <vrkljam@earthlink.net>
2023-03-20 17:07:18 -0600

    combined the apis and added disable to button

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 const btn = document.getElementById('btn')
+const button =document.querySelector('button')
+
 const audioElement = document.getElementById('audio')
+let counter =document.querySelector('.counter')
+let counterMinus =document.querySelector('.counter-minus')
+let counterPlus =document.querySelector('.counter-plus')
+let total=document.querySelector('.total')
+let modal =document.querySelector('.modal')
+let goodJokesModalButton =document.querySelector('.goodJokesModalButton')
+let closeBtn =document.querySelector('.closeBtn')
 
 // VoiceRSS Javascript SDK
 const VoiceRSS={speech:function(e){this._validate(e),this._request(e)},_validate:function(e){if(!e)throw"The settings are undefined";if(!e.key)throw"The API key is undefined";if(!e.src)throw"The text is undefined";if(!e.hl)throw"The language is undefined";if(e.c&&"auto"!=e.c.toLowerCase()){var a=!1;switch(e.c.toLowerCase()){case"mp3":a=(new Audio).canPlayType("audio/mpeg").replace("no","");break;case"wav":a=(new Audio).canPlayType("audio/wav").replace("no","");break;case"aac":a=(new Audio).canPlayType("audio/aac").replace("no","");break;case"ogg":a=(new Audio).canPlayType("audio/ogg").replace("no","");break;case"caf":a=(new Audio).canPlayType("audio/x-caf").replace("no","")}if(!a)throw"The browser does not support the audio codec "+e.c}},_request:function(e){var a=this._buildRequest(e),t=this._getXHR();t.onreadystatechange=function(){if(4==t.readyState&&200==t.status){if(0==t.responseText.indexOf("ERROR"))throw t.responseText;audioElement.src=t.responseText,audioElement.play()}},t.open("POST","https://api.voicerss.org/",!0),t.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=UTF-8"),t.send(a)},_buildRequest:function(e){var a=e.c&&"auto"!=e.c.toLowerCase()?e.c:this._detectCodec();return"key="+(e.key||"")+"&src="+(e.src||"")+"&hl="+(e.hl||"")+"&r="+(e.r||"")+"&c="+(a||"")+"&f="+(e.f||"")+"&ssml="+(e.ssml||"")+"&b64=true"},_detectCodec:function(){var e=new Audio;return e.canPlayType("audio/mpeg").replace("no","")?"mp3":e.canPlayType("audio/wav").replace("no","")?"wav":e.canPlayType("audio/aac").replace("no","")?"aac":e.canPlayType("audio/ogg").replace("no","")?"ogg":e.canPlayType("audio/x-caf").replace("no","")?"caf":""},_getXHR:function(){try{return new XMLHttpRequest}catch(e){}try{return new ActiveXObject("Msxml3.XMLHTTP")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.6.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.3.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP")}catch(e){}try{return new ActiveXObject("Microsoft.XMLHTTP")}catch(e){}throw"The browser does not support HTTP request"}};
+  
+//  disable/enable button
+function toggleButton(){
+    button.disabled = !button.disabled;
+}
+    // passing the joke to voice rss api
+function tellMe(joke){
+    VoiceRSS.speech({
+        key: 'bea472add29e45ab884f0a518e8d3354',
+        src: joke,
+        hl: 'en-us',
+        v: 'Linda',
+        r: 0, 
+        c: 'mp3',
+        f: '44khz_16bit_stereo',
+        ssml: false
+    });
+}
 
-// const test =()=> {
-//     VoiceRSS.speech({
-//         key: 'bea472add29e45ab884f0a518e8d3354',
-//         src: 'Hello, the world!',
-//         hl: 'en-us',
-//         v: 'Linda',
-//         r: 0, 
-//         c: 'mp3',
-//         f: '44khz_16bit_stereo',
-//         ssml: false
-//     });
-// }
-// test()
 
 // get jokes from joke api
 async function getJokes(){
@@ -30,7 +44,10 @@ async function getJokes(){
         } else {
             joke = data.joke
         }
-        console.log(joke)
+        // text to speech
+        tellMe(joke)
+        // disable button
+        toggleButton();
     }
     catch (err){
         // catch errors
@@ -39,13 +56,6 @@ async function getJokes(){
 }
 getJokes(); 
 
-let counter =document.querySelector('.counter')
-let counterMinus =document.querySelector('.counter-minus')
-let counterPlus =document.querySelector('.counter-plus')
-let total=document.querySelector('.total')
-let modal =document.querySelector('.modal')
-let goodJokesModalButton =document.querySelector('.goodJokesModalButton')
-let closeBtn =document.querySelector('.closeBtn')
 
 let count =0
 
@@ -55,29 +65,30 @@ function updateCounter(){
 const subtract = () => {
     count--;
     updateCounter()}
-
-const addition = () => {
-    count++;
-    updateCounter();}
-
-const openModal =() =>{
-    modal.style.display='block'
-}  
-
-const closeModalBtn =() =>{
-    modal.style.display='none'
-    }
-
-const clickOutside =(e) =>{
-    if(e.target == modal){
-    modal.style.display='none'}
-}
-
+    
+    const addition = () => {
+        count++;
+        updateCounter();}
+        
+        const openModal =() =>{
+            modal.style.display='block'
+        }  
+        
+        const closeModalBtn =() =>{
+            modal.style.display='none'
+        }
+        
+        const clickOutside =(e) =>{
+            if(e.target == modal){
+                modal.style.display='none'}
+            }
+            
+btn.addEventListener('click', getJokes);
+audioElement.addEventListener('ended',toggleButton)
 counterMinus.addEventListener('click',subtract)
 counterPlus.addEventListener('click',addition)
 goodJokesModalButton.addEventListener('click', openModal)
-// modal.addEventListener('click',)
 closeBtn.addEventListener('click', closeModalBtn)
 window.addEventListener('click', clickOutside)
-
-// !!!!!!!!!!add sessionStorage
+            
+            // !!!!!!!!!!add sessionStorage
